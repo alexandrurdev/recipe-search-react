abe836901c49b0dd928259ff8858a0e6768904fe
alexandrurdev <contact@ardev.ro>
2019-09-29 13:11:28 +0300

    Updates

---
+++ precache-manifest.f8538cb15ead7ef583ab90a8cd9905b5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0f00f4f13092dd6863580bea4f985e",
+    "url": "/recipe-search-react/index.html"
+  },
+  {
+    "revision": "9cfbcabe2b0283f76e32",
+    "url": "/recipe-search-react/static/js/2.88af1de5.chunk.js"
+  },
+  {
+    "revision": "05f2dd0aeb58932e4663",
+    "url": "/recipe-search-react/static/js/main.53ef2179.chunk.js"
+  },
+  {
+    "revision": "0d8fbb8c1a8c23af3d6f",
+    "url": "/recipe-search-react/static/js/runtime-main.16c52719.js"
+  }
+]);
